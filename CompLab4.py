b21d7859fc5a912afcf9071351d3892dd17e0f90
silvanio45 <82683847+silvanio45@users.noreply.github.com>
2026-04-12 17:37:56 -0300

    Correção na questão resolvida
    Updated Newton's method for spherical tank volume calculation and improved error reporting.

--- CompLab4.py
+++ CompLab4.py
@@ -2,60 +2,38 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-# X0 = Chute inicial
-# Xi+1 = Xi - (f(Xi)/f'(Xi))
+R = 4
+V_desejado = 50
 
-def f_newton(x):
-    return x**3 + x - 1
+def f_newton(h):
+    return (math.pi * h**2 / 3) * (3*R - h) - V_desejado
 
-def df_newton(x):
-    return 3*x**2 + 1
-
-def rf_fixed_point_convergente(x):
-    return 1 / (x**2 + 1)
+def df_newton(h):
+    return math.pi * h * (2*R - h)
 
 def method_Newton(f_newton, df_newton, X_inicial, Tolerancia, Max_Iteracoes):
-    ''' Para a equação x^3 + x + 1 Faremos:
-    1 - Colcar o chute inicial na reta tangente e ver onde ela cruza o eixo x
-    2 - Colocar o novo chute na reta tangente e ver onde ela cruza o eixo x
-    3 - e seguir nos passos 1 e 2 até que a condição de parada seja satisfeita
+    ''' Para o problema do tanque esférico:
+    1 - Colocar o chute inicial na reta tangente
+    2 - Encontrar o novo ponto
+    3 - Repetir até satisfazer a condição
     '''
     func_x = f_newton(X_inicial)
     der_func_x = df_newton(X_inicial)
 
     for i in range(Max_Iteracoes):
-        Xn = X_inicial - func_x/der_func_x
-        error = abs(Xn - X_inicial)
+        Xn = X_inicial - func_x / der_func_x
+        
+        # erro relativo (%)
+        error = abs((Xn - X_inicial) / Xn) * 100
 
         if error <= Tolerancia:
             print(f'A raiz aproximada é: {Xn:.8f}')
             break
 
-        print(f'i: {i} | error: {error:.2e} | X: {Xn:.8f}')
+        print(f'i: {i+1} | error: {error:.6f}% | X: {Xn:.8f}')
+        
         X_inicial = Xn
         func_x = f_newton(X_inicial)
         der_func_x = df_newton(X_inicial)
 
-
-def method_fixed_point(f_fixed_point, rf_fixed_point, X_inicial, Tolerancia, Max_Iteracoes):
-    for i in range(Max_Iteracoes):
-        X1 = rf_fixed_point(X_inicial)
-        error = abs(X1 - X_inicial)
-
-        print(f'i: {i} | error: {error:.2e} | X: {X1:.8f}')
-
-        if error <= Tolerancia:
-            print(f'\nA raiz aproximada é: {X1:.8f}')
-            break
-
-        X_inicial = X1
-    else:
-        print(f'Não convergiu em {Max_Iteracoes} iterações. Último X: {X1:.8f}')
-
-method_Newton(f_newton, df_newton, 0, 0.000000000000000001, 10)
-method_fixed_point(f_fixed_point, rf_fixed_point_convergente, 0.5, 0.001, 20)
-
-
-
-
-  
+method_Newton(f_newton, df_newton, 2, 0, 3)
